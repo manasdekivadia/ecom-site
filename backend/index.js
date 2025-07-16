a7e94033e5bf2b3bc91b1839a1ea45dbd8a48f66
manas <manas1729@gmail.com>
2025-07-17 00:27:01 +0300

    fixed vercel.json 1.1

--- backend/index.js
+++ backend/index.js
@@ -13,21 +13,9 @@ import {authRouter} from "./controllers/authController.js";
 config();
 
 const app = express();
-const allowedOrigins = [
-  'https://ecom-site-tlkb.vercel.app',
-  'https://ecom-site-tlkb-manas-dekivadias-projects.vercel.app' // your real deployed frontend URL on Vercel
-];
 
-app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-}));
+
+app.use(cors());
 
 
 
--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,25 +1,24 @@
-import {config} from "dotenv";
+import { config } from "dotenv";
 import jwt from "jsonwebtoken";
 
 config();
 
-const auth = async(req,res,next) =>{
+const auth = async (req, res, next) => {
     const authHeader = req.headers['authorization'];
 
-    if(!authHeader  || !authHeader.startsWith('Bearer ')){
-        return res.stauts(401).json({msg:'NO Token , Authorization has been Denied'});
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({ msg: 'No Token, Authorization has been Denied' });
     }
 
     const token = authHeader.split(' ')[1];
     try {
-        
-        const decoded = jwt.verify(token,process.env.JWT_SECRET);
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
         next();
     } catch (error) {
-        console.error("Token Verification Error : ",error.message);
-        res.status(401).json({msg:'Token is not Valid'});
+        console.error("Token Verification Error:", error.message);
+        res.status(401).json({ msg: 'Token is not Valid' });
     }
 };
 
-export {auth};
+export { auth };
